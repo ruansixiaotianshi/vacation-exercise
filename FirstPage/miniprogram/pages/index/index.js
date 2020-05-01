f72adbb317efa7a35930da3bda8752f58954db8b
ruansixiaotianshi <1916708674@qq.com>
2020-05-01 23:59:12 +0800

    2020.05.01日提交

---
+++ FirstPage/miniprogram/pages/index/index.js
@@ -0,0 +1,16 @@
+// pages/index/index.js
+//index.js  
+//获取应用实例  
+var app = getApp()  
+Page({  
+  data: {  
+    movies:[  
+      {url:"/images/1.jpg"},
+      {url:"/images/2.jpg"},
+      {url:"/images/3.jpg"}  
+    ]  
+  },  
+  onLoad: function () {  
+  }  
+})  
+  
